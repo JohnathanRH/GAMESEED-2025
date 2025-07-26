2e74fc9bbc5d9c0eeeedb11e8e832b9ece6fa393
JohnathanRH <144834953+JohnathanRH@users.noreply.github.com>
2025-07-27 01:56:31 +0800

    Revert "Player, Enemy, and Saves"
    This reverts commit 2f024210b13790c6e5d98cbb81bffafb571487ea.

--- Scripts/Resources/enemy/enemy.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name EnemyResource
-
-@export var animation : SpriteFrames
-@export var hp : int
-@export var atk_interval : float
--- Scripts/player.gd
+++
@@ -1,8 +0,0 @@
-extends AnimatedSprite2D
-
-# still empty, all stats and changes towards them are managed in the PlayerSaveFile resource
-
-func _input(event: InputEvent) -> void:
-	# Save, for debugging only
-	if(event.is_action_pressed("save")):
-		PlayerVariables.save_data()
