2b30313e8abf807ce217b3d1158983d57029c1a1
Gunturadhtya <153581523+Gunturadhtya@users.noreply.github.com>
2025-07-30 16:50:54 +0800

    a lot of improvment
    flip card sound added
    layout changed
    card shuffle randomizer algortihm revamped
    attack time slowed
    progress bar asset changd

--- Scripts/gds_stats_placeholder.gd
+++ Scripts/gds_stats_placeholder.gd
@@ -1,20 +1,16 @@
 extends Node
 
 @onready var player_save = PlayerVariables.save_file as PlayerSaveFile    # Get the player save file thru the autoloader
+@onready var player_hp = player_save.hp
 
 func _ready() -> void:
 	# Setup the ui upon entering the scene
-	$player_name.text = "Player Name: " + player_save.name
+	$player_name.text = player_save.name
 	update_hp()
-	update_level()
 	
 	# Connect these signals to detect changes on the respective attributes. (example: receiving damage)
 	player_save.hpSet.connect(update_hp)
-	player_save.levelSet.connect(update_level)
 
 # Functions to update the ui
 func update_hp() -> void:
-	$hp.text = "Player HP: " + str(player_save.hp)
-
-func update_level() -> void:
-	$level.text = "Player Level: " + str(player_save.level)
+	$hp.text = str(player_save.hp) + "/" + str(player_hp)
